d5120044b21bbfeb7bc1379b02caa3e144c5ab59
olusholapacheco Pacheco <sholapacheco@gmail.com>
2025-04-15 14:40:36 +0000

    region update for VPC

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_key_pair" "bootstrap_k8s_key_pair" {
 
 # Get VPC
 data "aws_vpc" "selected" {
-  id = "vpc-0e519291d11e7d07a"
+  id = "vpc-026038abecbf503ed"
 }
 
 # Create security group
